2ffbcf622417f27b3c7c0bd554f80957dbfed327
Yaro Plotnik <yaroslav.plotnik@gmail.com>
2024-08-05 11:16:35 +0300

    Release v6.1.1

--- Observer/TrackOrders.php
+++ Observer/TrackOrders.php
@@ -2,35 +2,47 @@
 
 namespace IWD\Opc\Observer;
 
+use Exception;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use IWD\Opc\Model\Order\SendOrderInformation as OrderInformation;
 
-
+/**
+ * Class TrackOrders
+ *
+ * This class implements the ObserverInterface and is responsible
+ * for tracking orders and sending order information using OrderInformation class.
+ *
+ * @package IWD\Opc\Observer
+ */
 class TrackOrders implements ObserverInterface
 {
-    /**
-     * @var OrderInformation
-     */
-    private $orderInformation;
 
-    /**
-     * @param OrderInformation $orderInformation
-     */
-    public function __construct(
-        OrderInformation $orderInformation
-    )
-    {
-        $this->orderInformation = $orderInformation;
-    }
+	/**
+	 * @var OrderInformation
+	 */
+	private $orderInformation;
 
-    /**
-     * @param Observer $observer
-     */
-    public function execute(Observer $observer)
-    {
-        $order = $observer->getEvent()->getOrder();
-        $this->orderInformation->sendOrderInformation($order);
+	/**
+	 * @param OrderInformation $orderInformation
+	 */
+	public function __construct(
+		OrderInformation $orderInformation
+	) {
+		$this->orderInformation = $orderInformation;
+	}
 
-    }
+	/**
+	 * @param Observer $observer
+	 */
+	public function execute(Observer $observer)
+	{
+		try {
+			$order = $observer->getEvent()->getOrder();
+
+			$this->orderInformation->sendOrderInformation($order);
+		} catch (Exception $e) {
+			// Skip sending Order Information
+		}
+	}
 }
